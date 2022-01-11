59851bb81d8c1fd41b28ccf6e9e632c558f363a0
xin <xin.li.phy@gmail.com>
2022-01-11 09:13:03 -0500

    Initial Commit

---
+++ derivs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  derivs
+//
+//  Created by Xin Li on 1/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
