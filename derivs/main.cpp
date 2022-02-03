4842194c2af3cbb637ecf327d20484a4f320a2c1
xin <xin.li.phy@gmail.com>
2022-02-03 11:05:04 -0500

    an example using boost lib

--- derivs/main.cpp
+++ derivs/main.cpp
@@ -7,14 +7,17 @@
 #include <iostream>
 #include "test.hpp"
 
+#include <boost/math/special_functions/erf.hpp>
+
+
 int main(int argc, const char * argv[]) {
-    
     std::cout << "main function get called! \n";
     //test_simpleMC();
     //test_exoticEngine();
     //test_tree();
     //test_solver();
     //test_factory();
+    std::cout << boost::math::erf(0.5);
     
     
     double tmp;
